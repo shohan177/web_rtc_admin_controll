b565fcd53a01e482e83cfcb43259407c2ea02db1
Sarwar Jahan Shohan <sarwar.shohan24@gmail.com>
2022-07-24 15:44:18 +0600

    firebase config update

--- src/firebase.js
+++ src/firebase.js
@@ -6,13 +6,13 @@ import { getDatabase } from "firebase/database";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBA2uo-53deRIPjKrdV_mOH_UBMihGDEIw",
-    authDomain: "livevideocall-updated.firebaseapp.com",
-    databaseURL: "https://livevideocall-updated-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "livevideocall-updated",
-    storageBucket: "livevideocall-updated.appspot.com",
-    messagingSenderId: "19673148748",
-    appId: "1:19673148748:web:7085f8836af31ec83f73e9"
+    apiKey: "AIzaSyBTGg1MleH-nQbBgK9i-qR3Qqo_DzQ1CQc",
+    authDomain: "final-video-call.firebaseapp.com",
+    databaseURL: "https://final-video-call-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "final-video-call",
+    storageBucket: "final-video-call.appspot.com",
+    messagingSenderId: "85509145950",
+    appId: "1:85509145950:web:7cd57a5f6939ebac763bcf"
 };
 
 // Initialize Firebase
